91d4fbb97e01d8a600422f5a07b51748e4debb65
Thinh M. Le <thinh.m.le@gmail.com>
2017-02-24 23:15:53 -0800

    Initial Commit

---
+++ RestKitWoutCoreData/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RestKitWoutCoreData
+//
+//  Created by Thinh Le on 2017-02-24.
+//  Copyright © 2017 Lac Viet Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RestKitWoutCoreData/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RestKitWoutCoreData
+//
+//  Created by Thinh Le on 2017-02-24.
+//  Copyright © 2017 Lac Viet Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
